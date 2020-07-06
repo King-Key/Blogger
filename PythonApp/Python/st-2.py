d2e9116ab9b99bceda95e66f4d01ebda1416e984
King-Key <guo_wang_113@163.com>
2020-07-07 07:39:44 +0800

    2020-07-07

--- PythonApp/Python/st-2.py
+++ PythonApp/Python/st-2.py
@@ -7,21 +7,24 @@
 
 
 import streamlit as st
-import cv2
+# import cv2
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == True:
+# while(cap.isOpened()):
+#     ret, frame = cap.read()
+#     if ret == True:
 
-        #cv2.imshow("vedio", frame)
-        st.image(frame)
+#         #cv2.imshow("vedio", frame)
+#         st.image(frame)
 
-        # vedio write
-        #out.write(frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-cap.release()
-# out.release()
-#cv2.destroyAllWindows()
+#         # vedio write
+#         #out.write(frame)
+#         if cv2.waitKey(1) & 0xFF == ord("q"):
+#             break
+# cap.release()
+# # out.release()
+# #cv2.destroyAllWindows()
+
+button=st.button("input")
+st.write(button)
